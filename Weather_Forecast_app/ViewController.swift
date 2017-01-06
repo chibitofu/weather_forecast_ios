26756f8264e373545263ce57e5aae3309a2c0ed3
chibitofu <erinmoony@gmail.com>
2017-01-05 20:10:12 -0800

    Initial Commit

---
+++ Weather_Forecast_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather_Forecast_app
+//
+//  Created by Erin Moon on 1/5/17.
+//  Copyright © 2017 Erin Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
